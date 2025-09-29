f1b6acedb2cef6cac798148a8164873233574a0b
krishnapriya U.A <krishnapriyaua@gmail.com>
2025-09-29 19:17:39 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+
+import { Button } from "@mui/material"
+
+function App() {
+  return (
+    <>
+     <div className="row container m-5">
+      <div className="col-md-8">
+        <p>hello there</p>
+      </div>
+      <div className="col-md-4">
+        <Button variant="contained" size="small">click here</Button>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
